91e4d9dc54cc364e67f2bed62ff634c33acf30e9
bishoe01 <bishoe01@gmail.com>
2025-06-24 21:20:58 +0900

    init: 프로젝트 생성

---
+++ TrapicJam/TrapicJam/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  TrapicJam
+//
+//  Created by bishoe on 6/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {}
+            .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrapicJam/TrapicJam/TrapicJamApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrapicJamApp.swift
+//  TrapicJam
+//
+//  Created by bishoe on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct TrapicJamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
